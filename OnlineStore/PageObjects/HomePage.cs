722f7cb787e71f2848f99f727fa80e901079aaef
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-30 16:06:28 -0700

    write PageObjects and create login test

---
+++ OnlineStore/PageObjects/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace OnlineStore.PageObjects
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using ="account")]
+        public IWebElement MyAccount { get; set; }
+    }
+}
---
+++ OnlineStore/PageObjects/LogInTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using NUnit.Framework;
+using OnlineStore.PageObjects;
+
+namespace OnlineStore.PageObjects
+{
+    class LogInTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.store.demoqa.com";
+
+            var homePage = new HomePage();
+            PageFactory.InitElements(driver, homePage);
+            homePage.MyAccount.Click();
+
+            var loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            loginPage.UserName.SendKeys("TestUser_1");
+            loginPage.Password.SendKeys("Test@123");
+            loginPage.Submit.Submit();
+        }
+    }
+}
