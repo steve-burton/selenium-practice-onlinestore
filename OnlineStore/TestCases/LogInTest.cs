c1d2077303c4ce528e5b8bde77d4e3766d206947
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-30 15:17:54 -0700

    write practice test for login

---
+++ OnlineStore/TestCases/LogInTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace OnlineStore.TestCases
+{
+    class LogInTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.store.demoqa.com";
+
+            driver.FindElement(By.XPath(".//*[@id='account']/a")).Click();
+
+            driver.FindElement(By.Id("log")).SendKeys("testuser_1");
+
+            driver.FindElement(By.Id("pwd")).SendKeys("Test@123");
+
+            driver.FindElement(By.Id("login")).Click();
+
+            driver.FindElement(By.XPath(".//*[@id='account_logout']/a")).Click();
+
+            driver.Quit();
+        }
+    }
+}
